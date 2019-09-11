aed40b8329477b3cf710b21aa019e3929410627f
Nick <bourdakos1@gmail.com>
2019-09-11 16:36:35 -0400

    fix issues

--- src/index.js
+++ src/index.js
@@ -105,10 +105,10 @@ const runPrediction = async (graph, labels, input) => {
   )
 }
 
-module.exports = {
+export default {
   load: async path => {
-    const graphPath = path
-    const labelsPath = path
+    const graphPath = path + '/model.json'
+    const labelsPath = path + '/labels.json'
     const graphPromise = tf.loadGraphModel(graphPath)
     const labelsPromise = fetch(labelsPath).then(data => data.json())
     const [graph, labels] = await Promise.all([graphPromise, labelsPromise])
